ecdd0a1553fbed77890d82b355bcf0c58d84de97
Satyam Rai <satyamrai0911@gmail.com>
2022-05-31 19:42:29 +0530

    Final Commit

--- Capstone-Backend-Integration-Eshop-/index.js
+++ Capstone-Backend-Integration-Eshop-/index.js
@@ -3,9 +3,10 @@ const connect = require("./config/db");
 const PORT = 8000;
 const app = express();
 var cors = require("cors");
-//Routes
+
 
 const userRoutes = require("./routes/api/UserRoute");
+const productRoutes = require("./routes/api/ProductRoutes");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,5 +15,6 @@ app.use(cors());
 connect();
 
 app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => console.log("the server is started ✈"));
+app.listen(PORT, () => console.log("the server is started "));
